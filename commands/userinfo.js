dc348f4536f5c68af32c6d62a59533fd7f6b8e97
smoki1337 <69118638+smoki1337@users.noreply.github.com>
2020-10-16 22:24:35 +0300

    Delete userinfo.js

--- commands/userinfo.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'userinfo',
-    description: 'Provides information about the user',
-    execute(message, args){
-        message.channel.send(`Username: ${message.author.username}\nUser ID: ${message.author.id}`);
-    },
-};
